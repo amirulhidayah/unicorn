2abd7d8114b9340d1caf155eade2debbc67ff47f
amirulhidayah <hidayahamirul103@gmail.com>
2022-06-29 15:10:07 +0800

    Fix Summernote

--- app/Http/Controllers/dashboard/masterData/TentangController.php
+++ app/Http/Controllers/dashboard/masterData/TentangController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\dashboard\masterData;
 use App\Http\Controllers\Controller;
 use App\Models\Tentang;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 
@@ -32,7 +33,31 @@ class TentangController extends Controller
             return response()->json(['error' => $validator->errors()]);
         }
 
-        $tentang->isi = $request->isi;
+        $content = $request->isi;
+        $dom = new \DomDocument();
+        $dom->loadHtml($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        $imageFile = $dom->getElementsByTagName('img');
+
+        foreach ($imageFile as $item => $image) {
+            $data = $image->getAttribute('src');
+            $extension = explode('/', mime_content_type($data))[1];
+            list($type, $data) = explode(';', $data);
+            list(, $data)      = explode(',', $data);
+            $imgeData = base64_decode($data);
+
+            $image_name = 'tentang/' . time() . '.' . $extension;
+            $path = $image_name;
+            Storage::put($path, $imgeData);
+
+            $urlSrc = Storage::url($image_name);
+
+            $image->removeAttribute('src');
+            $image->setAttribute('src', $urlSrc);
+        }
+
+        $content = $dom->saveHTML();
+
+        $tentang->isi = $content;
         $tentang->save();
 
         return response()->json(['status' => 'success']);
